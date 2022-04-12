5197eb9e7a4f5c08314de5f71a06a6ab81a025db
LordUttam <sharmaji.uttam@gmail.com>
2022-04-13 00:59:31 +0530

    Fix: Router fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import { LandingPage } from "./pages";
+import { Route, Routes } from "react-router-dom";
+import { LandingPage } from "pages";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
-    <Router />
-    <App />
-    <Router />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
