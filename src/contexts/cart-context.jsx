8924238b39e2e2f64a6e231f1bd87fb374e6be42
LordUttam <sharmaji.uttam@gmail.com>
2022-05-22 22:35:17 +0530

    Fix: removing redundant braces

--- src/contexts/cart-context.jsx
+++ src/contexts/cart-context.jsx
@@ -1,13 +1,10 @@
 import { createContext, useContext, useReducer, useEffect } from "react";
 import { cartReducer } from "reducers/cart-reducer";
-import { getCartApi } from "operations";
-import { useAuth } from "./auth-context";
 const defaultCartValue = { cartItems: [] };
 const CartContext = createContext(defaultCartValue);
 
 function CartProvider({ children }) {
   const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartValue);
-  const { authData } = useAuth();
   useEffect(() => {
     let cart = JSON.parse(localStorage.getItem("cart"));
     cartDispatch({ type: "SET_CART", payload: cart || [] });
--- src/reducers/wishlist-reducer.js
+++ src/reducers/wishlist-reducer.js
@@ -1,25 +1,21 @@
 const wishlistReducer = (wishlistState, action) => {
   if (action.type === "ADD_TO_WISHLIST") {
-    {
-      let wishlist = [...wishlistState.wishlistItems, { ...action.payload }];
-      localStorage.setItem("wishlist", JSON.stringify(wishlist));
-      return {
-        ...wishlistState,
-        wishlistItems: wishlist,
-      };
-    }
+    let wishlist = [...wishlistState.wishlistItems, { ...action.payload }];
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    return {
+      ...wishlistState,
+      wishlistItems: wishlist,
+    };
   }
   if (action.type === "REMOVE_FROM_WISHLIST") {
-    {
-      let wishlist = wishlistState.wishlistItems.filter(
-        (product) => product._id !== action.payload._id
-      );
-      localStorage.setItem("wishlist", JSON.stringify(wishlist));
-      return {
-        ...wishlistState,
-        wishlistItems: wishlist,
-      };
-    }
+    let wishlist = wishlistState.wishlistItems.filter(
+      (product) => product._id !== action.payload._id
+    );
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    return {
+      ...wishlistState,
+      wishlistItems: wishlist,
+    };
   }
   if (action.type === "SET_WISHLIST") {
     return {
